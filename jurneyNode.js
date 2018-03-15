3cdf416a4aab27ca7cb8b37a0dbdc87f9920b3e7
yxnne <506524339@qq.com>
2018-03-15 22:44:59 +0800

    添加handlebars 渲染视图引擎，以及express静态文件中间件，均测试可用

--- jurneyNode.js
+++ jurneyNode.js
@@ -2,32 +2,43 @@ var express = require('express');
 
 var app = express();
 
+// 添加 handlebars 视图引擎
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// 添加静态文件资源处理中间件
+app.use(express.static(__dirname + '/public' ));
+
 // 设置端口
 app.set('port', process.env.PORT || 3000);
 
 app.get('/',function(req, res){
-	res.type('text/plain');
-	res.send('OK,This is Root');
+	// res.type('text/plain');
+	// res.send('OK,This is Root');
+	res.render('home');
 });
 
 app.get('/about',function(req, res){
-	res.type('text/plain');
-	res.send('About Me');
+
+	// 测试handlebars渲染动态值
+	const names = ['yxnne', 'ax', 'jerkins', 'yellowman', 'yaplob'];
+	var name = names[Math.floor( Math.random() * names.length)];
+	res.render('about', {randomName:name});
 });
 
 // 定制404页面
 app.use(function(req, res){
-	res.type('text/plain');
+	//res.type('text/plain');
 	res.status(404);
-	res.send('404 - Not Found');
+	res.render('404');
 });
 
 // 定制500页面
 app.use(function(err, req, res, next){
 	console.error(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
+	res.render('500');
 });
 
 app.listen(app.get('port'), function(){
